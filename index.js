d70c94efe6107c4f92d5d7393d87dfad65b777b5
saumitra-jha <81382947+saumitra-jha@users.noreply.github.com>
2023-08-31 00:04:28 +0530

    index.js

---
+++ index.js
@@ -0,0 +1,55 @@
+{//basic output
+    console.log("Saumitra Jha");
+}
+{// simple math
+const number1 = 4;
+const number2 = 9;
+sum = number1+ number2;
+divide = number1 / number2; 
+multiply = number1 * number2;
+subtract = number1 - number2;
+console.log (sum);
+console.log (divide);
+console.log (multiply);
+console.log (subtract);
+}
+{ // using variables
+const city = "paris";
+console.log(city);
+ }
+ {
+const a = 10;
+const b = 14;
+ sum = a + b ;
+ console.log(sum);
+ }
+ { //function creation
+    function sayHello() {
+        const name = "What's your name?";
+        console.log(`Hello, ${name}!`);
+    }
+    sayHello();
+ }
+ { // math function
+function minusNumbers(a, b) {
+    let substract = a - b;
+    return substract;
+}
+
+let result = minusNumbers(10, 4);
+console.log(result); // Outputs: 6
+ }
+ { //advance variables
+   const birthYear = 2001;
+ }
+  {
+ const favouriteFood = [Biryani, Burritos, sweet  ]    
+  }
+  {
+  const book = {
+    title: " Pyjama Profit",
+    author: "Varun Mayya & Abhinav Chhikara",
+    publicationYear: 2018
+    }
+console.log(book)
+  }
